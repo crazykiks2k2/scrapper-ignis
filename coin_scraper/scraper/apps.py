6b74f7dc6097c25c0972eeb252046469f31fad1d
Kishore <kishorekiku2k2@gmail.com>
2024-06-09 11:21:19 +0530

    configure settings.py & models.py

--- coin_scraper/scraper/apps.py
+++ coin_scraper/scraper/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ScapperConfig(AppConfig):
+class ScraperConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'scapper'
+    name = 'scraper'
